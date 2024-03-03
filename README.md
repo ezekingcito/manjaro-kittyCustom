aaace9ae50c37ecfcd73989d235a4deb7f6bd6f9
Ezeking BB <emendoza_02@outlook.com>
2024-03-03 05:27:42 -0600

    v1

---
+++ README.md
@@ -0,0 +1,22 @@
+# arch-kitty-custom
+ Custom de kitty en Arch Linux
+
+
+
+Primero que nada actualiza el sistema antes de usar este escript con ```sudo pacman -Syy``` y ```sudo pacman -Syu```
+
+1.- Uso del Script 
+
+```
+git clone https://github.com/ezekingcito/arch-kitty-custom.git
+
+cd arch-kitty-custom/
+
+sudo chmod +x install.sh
+
+./install.sh
+```
+
+Este Script esta diseñado para funcional en Manjaro, aun que si decides probarlo en otra distrubicion dejamelo saber para ver que tal te fue y talvez darle soporte en la distribucion de tu eleccion, cabe aclarar que solo funciona en distribuciones base Arch Linux.
+
+#=================================================================================#
---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [ "$(whoami)" == "root" ]; then
+    exit 1
+fi
+
+ruta=$(pwd)
+
+#Actualizando Sistema
+sudo pacman -Syy
+sudo pacman -Syu
+
+#Instalando dependencias basicas
+sudo pacman -S curl wget neofetch
+
+# Instalando p10k
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Instalando p10k root
+
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
+
+
+# Instando lsd
+
+sudo pacman -S kitty zsh lsd
+
+# Instalamos las HackNerdFonts
+
+sudo mkdir /usr/local/share/fonts/
+
+sudo cp -v $ruta/fonts/HNF/* /usr/local/share/fonts/
+
+
+# Copiando Archivos de Configuración
+
+cp -rv $ruta/Config/kitty/ ~/.config/
+sudo cp -rv $ruta/kitty /opt/
+
+# Copia de configuracion de .p10k.zsh y .zshrc
+
+rm -rf ~/.zshrc
+cp -v $ruta/.zshrc ~/.zshrc
+
+cp -v $ruta/.p10k.zsh ~/.p10k.zsh
+sudo cp -v $ruta/.p10k.zsh-root /root/.p10k.zsh
+
+# Plugins ZSH
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+mv -v zsh-syntax-highlighting/ ~/
+
+sudo mkdir /usr/share/zsh-sudo
+cd /usr/share/zsh-sudo
+sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+
+echo "Terminado, eliminando repositorio"
+
+sudo rm -r $ruta
+
+notify-send "KITTY CUSTOM INSTALADO"
